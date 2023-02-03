f19dac8d5142b76c4aecfe648fe4058dfa89aa2a
Arman Dejbakhsh <arman@dejbakhsh.fr>
2023-02-03 15:36:32 +0100

    Tic tac to ugly code

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,40 +1,11 @@
 import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import './Button'
-import Button from './Button';
-import { useState } from 'react';
+import '../css/App.css';
+import Game from './Game'
 
 
 function App() {
-  const [count, setCount] = useState(0);
-
-
-  function handleClick() {
-      setCount(count + 1);
-    }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button  count={count} onClick={handleClick} />
-        <Button  count={count} onClick={handleClick} />
-
-
-      </header>
-    </div>
+    <Game/>
   );
 }
 
